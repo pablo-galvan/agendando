e9965387e4d172e2336af27684e788c7ca6fbfcc
Pablo Galvan <pablogalvan@MacBook-Pro-de-Pablo.local>
2017-10-21 22:29:57 -0300

    some errors and dependecies

--- server/config/modules/cache.js
+++ server/config/modules/cache.js
@@ -1,12 +1,12 @@
 'use strict';
 
 module.exports = {
-	enabled: true,
+	enabled: false,
 	http: {
 		enabled: false
 	},
 	redis: {
-		enabled: !!process.env.REDIS ? true : false,
+		enabled: false,
 		host: !!process.env.REDIS ? process.env.REDIS.host : '127.0.0.1',
 		port:!!process.env.REDIS ? process.env.REDIS.port : '6379',
         expire: {
